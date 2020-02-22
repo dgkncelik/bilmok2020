916237d55aacf1e5d8a7a73d0ea100315c859f68
Dogukan CELIK <dogukan.celik@itu.edu.tr>
2020-02-22 18:48:04 +0300

    Initial Commit

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -27,5 +27,20 @@ def get_area_shops(area_url_suffix):
         shops.append({"restaurantName": element.find("span").text, "restaurantLink": element.get("href")})
     return shops
 
+def get_shop_comments(trst):
+    comments = []
+    _request_url = 'https://www.yemeksepeti.com/pasa-doner-arnavutkoy-ilcesi-merkez-mah-istanbul?section=comments'
+    response = requests.get(_request_url)
+    soup = BeautifulSoup(response.text, "lxml")
+    all_comment_elements = soup.findAll("div", {"class": "comments-body"})
+    for element in all_comment_elements:
+        _comment = {"body": element.findAll("p")[0].text,
+                    "speed_point": element.findAll("div", {"class": "speed pointText p10"})[0].text,
+                    "serving_point": element.findAll("div", {"class": "serving pointText p10"})[0].text,
+                    "flavour_point": element.findAll("div", {"class": "flavour pointText p10"})[0].text
+                    }
+        comments.append(_comment)
+    return comments
+
 if __name__ == '__main__':
-    get_area_shops(YEMEK_SEPETI_CITY_URL)
+    get_shop_comments(YEMEK_SEPETI_CITY_URL)
