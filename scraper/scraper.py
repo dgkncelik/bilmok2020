1b1ce307b36f4b0369bb7264be52a1337aa2b717
Dogukan CELIK <dogukan.celik@itu.edu.tr>
2020-02-23 01:10:01 +0300

    Add class implementation for yemeksepeti scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -3,44 +3,65 @@ from bs4 import BeautifulSoup
 import lxml
 
 
-YEMEK_SEPETI_CITY_URL = 'https://www.yemeksepeti.com/istanbul'
+class YemekSepetiScraper(object):
+    def __init__(self):
+        self.yemeksepeti_url = 'https://www.yemeksepeti.com/'
+        self.city_name = 'istanbul'
 
+    def get_city_areas(self, city_url):
+        areas = []
+        response = requests.get(city_url)
+        soup = BeautifulSoup(response.text, "lxml")
+        all_option_tags = soup.findAll("option")
+        for option in all_option_tags:
+            area = {"name": "", "area_url": ""}
+            option_url = option.get("data-url")
+            if option_url and ("ilce" in option_url or "mah" in option_url):
+                area['area_url'] = option_url
+                areas.append(area)
+        return areas
 
-def get_areas(city_url):
-    urls = []
-    response = requests.get(city_url)
-    soup = BeautifulSoup(response.text, "lxml")
-    all_option_tags = soup.findAll("option")
-    for option in all_option_tags:
-        _option = option.get("data-url")
-        if _option and ('ilce' in _option or 'mah' in _option):
-            urls.append(option.get("data-url"))
-    return urls
+    def get_area_shops(self, area):
+        shops = []
+        request_url = area['area_url']
+        response = requests.get(request_url)
+        soup = BeautifulSoup(response.text, "lxml")
+        all_restaurant_elements = soup.findAll("a", {"class": "restaurantName"})
+        for element in all_restaurant_elements:
+            shop = {"restaurant_name": element.find("span").text,
+                    "restaurant_link":  element.get("href")}
+            shops.append(shop)
+        return shops
 
-def get_area_shops(area_url_suffix):
-    shops = []
-    _reqeust_url = 'https://www.yemeksepeti.com/istanbul/arnavutkoy-ilcesi-anadolu-mah'
-    response = requests.get(_reqeust_url)
-    soup = BeautifulSoup(response.text, "lxml")
-    all_restaurant_elements = soup.findAll("a", {"class": "restaurantName"})
-    for element in all_restaurant_elements:
-        shops.append({"restaurantName": element.find("span").text, "restaurantLink": element.get("href")})
-    return shops
+    def get_shop_comments(self, shop):
+        comments = []
+        request_url = '%s?section=comments' % shop['restaurant_link']
+        response = requests.get(request_url)
+        soup = BeautifulSoup(response.text, "lxml")
+        all_comment_elements = soup.findAll("div", {"class": "comments-body"})
+        for element in all_comment_elements:
+            comment = {"body": element.findAll("p")[0].text,
+                       "speed_point": element.findAll("div", {"class": "speed pointText p10"})[0].text,
+                       "flavour_point": element.findAll("div", {"class": "flavour pointText p10"})[0].text}
+            comments.append(comment)
+        return comments
 
-def get_shop_comments(trst):
-    comments = []
-    _request_url = 'https://www.yemeksepeti.com/pasa-doner-arnavutkoy-ilcesi-merkez-mah-istanbul?section=comments'
-    response = requests.get(_request_url)
-    soup = BeautifulSoup(response.text, "lxml")
-    all_comment_elements = soup.findAll("div", {"class": "comments-body"})
-    for element in all_comment_elements:
-        _comment = {"body": element.findAll("p")[0].text,
-                    "speed_point": element.findAll("div", {"class": "speed pointText p10"})[0].text,
-                    "serving_point": element.findAll("div", {"class": "serving pointText p10"})[0].text,
-                    "flavour_point": element.findAll("div", {"class": "flavour pointText p10"})[0].text
-                    }
-        comments.append(_comment)
-    return comments
+    def analyze_by_elasticsearch(self, comments):
+        return None
+
+    def index_to_elasticsearch(self, comments):
+        return None
+
+    def run(self):
+        city_url = '%s/%s' % (self.yemeksepeti_url, self.city_name)
+        areas = self.get_city_areas(city_url)
+        for area in areas:
+            shops = self.get_area_shops(area)
+            for shop in shops:
+                comments = self.get_shop_comments(shop)
+                comments = self.analyze_by_elasticsearch(comments)
+                self.index_to_elasticsearch(comments)
+
 
 if __name__ == '__main__':
-    get_shop_comments(YEMEK_SEPETI_CITY_URL)
+    YemekSepetiScraper().run()
